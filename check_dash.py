5239f8a163fd5a0416bd686a1eb25f7524c3bcf0
ShanBirch <shannonrhysbirch@gmail.com>
2026-02-26 09:53:46 +1000

    list dashboard file

--- check_dash.py
+++ check_dash.py
@@ -1,6 +1,2 @@
 import os
-print(os.path.getsize('dashboard.html'))
-with open('dashboard.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-    print(len(content))
-    print(content[:500])
+print(os.path.exists('dashboard.html'))
