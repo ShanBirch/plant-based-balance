9cdda1f7ca07786829ec1ae5ae3bde43a6361274
ShanBirch <shannonrhysbirch@gmail.com>
2026-02-26 07:18:13 +1000

    grep html

---
+++ grep_html.py
@@ -0,0 +1,8 @@
+import os
+with open('dashboard.html', 'r', encoding='utf-8') as f:
+    html = f.read()
+    print("Logout" in html)
+    print("Log out" in html)
+    print("Log Out" in html)
+    print("Sign Out" in html)
+    print("signout" in html)
