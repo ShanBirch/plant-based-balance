a4590f2a1ddeb5aa280b861aa9183f4fed60278a
Shanbot <shannonrhysbirch@gmail.com>
2026-09-13 17:14:05 +1000

    Remove redundant story introduction paragraph

--- tests/journey-intro-order.test.js
+++ tests/journey-intro-order.test.js
@@ -1,8 +1,8 @@
 const test=require('node:test'),assert=require('node:assert/strict'),fs=require('node:fs');
-test('story introduction follows the portrait and precedes the timeline',()=>{
+test('story portrait leads to timeline without the introductory paragraph',()=>{
  const html=fs.readFileSync('journey.html','utf8');
  const picture=html.indexOf('src="photos/shannon-portrait.jpg"');
- const intro=html.indexOf('I was plant-based from birth, raised');
- assert.ok(picture<intro);assert.ok(intro<html.indexOf('How it unfolded'));
- assert.equal(html.split('I was plant-based from birth, raised').length-1,1);
+ assert.ok(picture>=0);assert.ok(picture<html.indexOf('How it unfolded'));
+ assert.ok(!html.includes('I was plant-based from birth, raised'));
+ assert.ok(!html.includes('journey-introduction'));
 });
